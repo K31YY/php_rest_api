e278b8c03d579abcd6d9b42eea178209f754d135
kheourtsokhy <naknak6412@gmail.com>
2025-11-30 09:46:37 +0700

    changed file test

--- function.php
+++ function.php
@@ -5,51 +5,51 @@
         private $sql;
         private $result;
         // contructor 
-        function __destructconstruct()
+        function __construct()
         {
             require_once 'DbConnection.php';
             // creating an instance/object of class DbConnection
             $this->db = new DbConnection();
             // calling the connect method
-            $this->db = $this->db->connect();
+            $this->db->connect();
         }
+      
         // destructor
-        function __destructconstructdestruct()
+        function __destruct()
         {
             // closing the database connection
             $this->db->connect()->close();
         }
+      
         // method 
         public function insert_data($tablename, $fields, $values){
-            $countt = count($fields); // count fields in array
+            $count = count($fields); // count fields in array
             // generate INSERT statement
-            $this->sql = "INSERT INTI $tablename(";
-            for($i=0; $i < $countt; $i++){
+            $this->sql = "INSERT INTO $tablename(";
+            for($i=0; $i < $count; $i++){
                 $this->sql .= $fields[$i];
-                if($i < $countt - 1){
+                if($i < $count - 1){
                     $this->sql .= ",";
                 }else{
                     $this->sql .= ") VALUES (";
                 }
-                for($i=0; $i < $countt; $i++){
-                    $this->sql .= "'". $values[$i]. "'";
-                    if($i < $countt - 1){
-                        $this->sql .= ",";
-                    }else{
-                        $this->sql .= ");";
-                    }
-                }
-                
-                //execute INSERT statement
-                $this->result = $this->db->query($this->sql);
-                if($this->result === TRUE){
-                    return true;
-                }else{
-                    return false;
-                }
-                
             }
-            return $this->sql;
+            for($i=0; $i < $count; $i++){
+                $this->sql .= "'". $values[$i]. "'";
+                if($i < $count - 1){
+                    $this->sql .= ",";
+                }else{
+                    $this->sql .= ");";
+                }
+            }
+
+            //execute INSERT statement
+            $this->result = $this->db->connect()->query($this->sql);
+            if($this->result === TRUE){
+                return true;
+            }else{
+                return false;
+            } 
         }
     }
         
--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
     include 'function.php';
 
-    $fields = array("CategoryName", "cateImage");
-    $values = array("Beer", "beer.png");
+    // $fields = array("CategoryName", "cateImage","Description" );
+    // $values = array("Beer", "beer.png","Testing insert function");
 
-    // creating an instance/object of class functions
-    $func = new functions();
-    // calling the insert_data method
-    $test = $func->insert_data("tblcategories", $fields, $values);
+    // // creating an instance/object of class functions
+    // $func = new functions();
+    // // calling the insert_data method
+    // $test = $func->insert_data("tblcategories", $fields, $values);
     echo $test;
 ?>
